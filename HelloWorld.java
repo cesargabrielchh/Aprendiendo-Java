d8c91178d0cd39ca44b5748a737f95c3b5d80ba7
cesargabrielchh <cesar_gabriel89@hotmail.com>
2024-08-09 15:56:37 -0600

    Prueba

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+/*
+    Primer ejercicio para Java, la vieja confiable:
+    mi primer Hello world!
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
