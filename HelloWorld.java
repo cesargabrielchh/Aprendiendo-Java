29598b74d05a707d45204557c44c0258e755555f
cesargabrielchh <cesar_gabriel89@hotmail.com>
2024-08-09 15:59:03 -0600

    Prueba

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String message = "Hellow world!";
+
+        System.out.println(message);
     }
 }
