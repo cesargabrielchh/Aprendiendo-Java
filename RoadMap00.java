3510a3951acb41e088f506764d2ad7520e4d6203
cesargabrielchh <cesar_gabriel89@hotmail.com>
2024-08-09 16:29:54 -0600

    Otro comentario de prueba

---
+++ RoadMap00.java
@@ -0,0 +1,33 @@
+public class RoadMap00 {
+    /*
+     * Este primer ejercicio consiste en conocer cuál es la ágina oficial de Java,
+     * saber cómo se comenta y documenta el código, tipos de variables y conocer
+     * las variables primitivas.
+     */
+
+    public static void main(String[] args) {
+
+        /*
+         * Este es un comentario de varias líneas que se usa cuando es necesario
+         * explicar algún fragmento del código y que puede ocupar más de una
+         * línea para lograrlo.
+         */
+
+        // De esta manera es como se escribe un comentario corto
+
+        /**
+         * Este otro código es usado para documentar
+         */
+
+        //Variables primitivas
+        byte age = 21;
+        short students = 50;
+        int citizens = 30000;
+        long speedLight = 300000;
+        float stature = 1.80F;
+        double bigNumber = 32154654.1245;
+        char aSingleLetter = 'a';
+        boolean isThatTrue = false;
+
+    }
+}
