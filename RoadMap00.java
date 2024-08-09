8798d4547fb8f09dc497ebd306fe3d5a2890d71e
cesargabrielchh <cesar_gabriel89@hotmail.com>
2024-08-09 16:42:55 -0600

    Se termina los primeros ejercicios

--- RoadMap00.java
+++ RoadMap00.java
@@ -1,8 +1,26 @@
 public class RoadMap00 {
     /*
-     * Este primer ejercicio consiste en conocer cuál es la ágina oficial de Java,
-     * saber cómo se comenta y documenta el código, tipos de variables y conocer
-     * las variables primitivas.
+     * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+     * - Recuerda que todas las instrucciones de participación están en el
+     *   repositorio de GitHub.
+     *
+     * Lo primero... ¿Ya has elegido un lenguaje?
+     * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+     * - Este primer reto te servirá para familiarizarte con la forma de participar
+     *   enviando tus propias soluciones.
+     *
+     * EJERCICIO:
+     * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+     *   lenguaje de programación que has seleccionado.
+     * - Representa las diferentes sintaxis que existen de crear comentarios
+     *   en el lenguaje (en una línea, varias...).
+     * - Crea una variable (y una constante si el lenguaje lo soporta).
+     * - Crea variables representando todos los tipos de datos primitivos
+     *   del lenguaje (cadenas de texto, enteros, booleanos...).
+     * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+     *
+     * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+     * debemos comenzar por el principio.
      */
 
     public static void main(String[] args) {
@@ -11,6 +29,7 @@ public class RoadMap00 {
          * Este es un comentario de varias líneas que se usa cuando es necesario
          * explicar algún fragmento del código y que puede ocupar más de una
          * línea para lograrlo.
+         * El sitio web oficial de Java es: https://www.java.com/
          */
 
         // De esta manera es como se escribe un comentario corto
@@ -28,6 +47,14 @@ public class RoadMap00 {
         double bigNumber = 32154654.1245;
         char aSingleLetter = 'a';
         boolean isThatTrue = false;
+
+        //Variables no primitivas
+        String myName = "César Chairez";
+
+
+        //Constantes
+        final float PI_VALUE = 3.14159F;
+
 
     }
 }
